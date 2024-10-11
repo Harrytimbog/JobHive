1752055567ff04a39458865faf94524434e4a44a
harrytimbog <harriemannie@gmail.com>
2024-10-11 23:15:25 +0100

    Creating a user seeder class for seeding users

---
+++ JobHive/Data/UserSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JobHive.Data
+{
+    public class UserSeeder
+    {
+        public static async Task SeeedUserAsyn(IServiceProvider serviceProvider)
+        {
+            // Get a new instance of ApplicationDbContext for seeding purposes
+            using var scope = serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            userManager.
+
+        }
+    }
+}
