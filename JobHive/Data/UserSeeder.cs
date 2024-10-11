984f615cd426546d9e8d8f7563e246efb6a1b809
harrytimbog <harriemannie@gmail.com>
2024-10-11 23:36:41 +0100

    Seeding A User with a role

--- JobHive/Data/UserSeeder.cs
+++ JobHive/Data/UserSeeder.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using JobHive.Constants;
+using Microsoft.AspNetCore.Identity;
 
 namespace JobHive.Data
 {
     public class UserSeeder
     {
-        public static async Task SeeedUserAsyn(IServiceProvider serviceProvider)
+        public static async Task SeedUserAsync(IServiceProvider serviceProvider)
         {
             // Get a new instance of ApplicationDbContext for seeding purposes
             using var scope = serviceProvider.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-            userManager.
+            if (await userManager.FindByEmailAsync("admin@jobhive.com") == null)
+            {
+                var user = new IdentityUser
+                {
+                    Email = "admin@jobhive.com",
+                    EmailConfirmed = true,
+                    UserName = "admin@jobhive"
+                };
 
-        }
+                await userManager.CreateAsync(user, "Admin@123!");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, Roles.Admin);
+                }
+            }
     }
 }
