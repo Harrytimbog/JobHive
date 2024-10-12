cfb9f326553ae05d7f8419f5b25cdaac53649c8c
harrytimbog <harriemannie@gmail.com>
2024-10-12 23:29:25 +0100

    Executing unit tests and the test runner

--- JobHive.Tests/JobPostingRepositoryTests.cs
+++ JobHive.Tests/JobPostingRepositoryTests.cs
@@ -1,4 +1,7 @@
 ﻿using JobHive.Data;
+using JobHive.Models;
+using JobHive.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,7 +13,9 @@ namespace JobHive.Tests
 {
     internal class JobPostingRepositoryTests
     {
-        private readonly DbContextOptions<ApplicationDbContext> _options;
+        // Only a public class can be tested
+
+        public readonly DbContextOptions<ApplicationDbContext> _options;
 
         public JobPostingRepositoryTests()
         {
@@ -22,6 +27,33 @@ namespace JobHive.Tests
         // creating an instance of the ApplicationDbContext
         private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
 
+        // Test for Add JobPosting Async
+        public async Task AddAsync_ShouldAddJobPosting()
+        {
+            // db context
+            var db = CreateDbContext();
+            // job posting repository
+            var jobPostingRepository = new JobPostingRepository(db);
+            // job posting
 
+            var jobPosting = new JobPosting
+            {
+                Title = "Software Developer",
+                Description = "Develop software",
+                Location = "Lagos",
+                Company = "Andela",
+                UserId = "1",
+                User = new IdentityUser { Id = "1", UserName = "testuser" } // Set the required User property
+            };
+
+            // expected job posting created
+            await jobPostingRepository.AddAsync(jobPosting);
+            // result job posting created
+
+            var result = db.JobPostings.FirstOrDefault(j => j.Title == "Software Developer");
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(jobPosting.Title, result.Title);
+        }
     }
 }
