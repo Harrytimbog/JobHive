2ff4df8585ed54266f07aeb82b919a23f9cbf658
harrytimbog <harriemannie@gmail.com>
2024-10-12 21:51:55 +0100

    Setting up a unit test project

---
+++ JobHive.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using JobHive.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHive.Tests
+{
+    internal class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("JobPostingDb")
+                .Options;
+        }
+
+        // creating an instance of the ApplicationDbContext
+        private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+
+
+    }
+}
