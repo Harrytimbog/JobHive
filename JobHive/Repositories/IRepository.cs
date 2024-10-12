f66fd2acc80bc9f3ed194c91cc8f2e85751f9bc0
harrytimbog <harriemannie@gmail.com>
2024-10-12 20:41:59 +0100

    Building the generic Repository Interface

---
+++ JobHive/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace JobHive.Repositories
+{
+    // Interface for every kind of Repository
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+    }
+}
