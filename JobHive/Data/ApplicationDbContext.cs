a6ed5554b02cdae217e34402e7549be89b5fdce8
harrytimbog <harriemannie@gmail.com>
2024-10-12 11:48:47 +0100

    JobPosting migration

--- JobHive/Data/ApplicationDbContext.cs
+++ JobHive/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using JobHive.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobHive.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<JobPosting> JobPostings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- JobHive/Models/JobPosting.cs
+++ JobHive/Models/JobPosting.cs
@@ -9,25 +9,25 @@ namespace JobHive.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Title { get; set; }
+        public required string Title { get; set; }
         [Required]
-        public string Description { get; set; }
+        public required string Description { get; set; }
         [Required]
-        public string Location { get; set; }
+        public required string Location { get; set; }
         [Required]
-        public string Company { get; set; }
+        public required string Company { get; set; }
 
         public DateTime PostedDate { get; set; } = DateTime.UtcNow;
 
         public bool IsApproved { get; set; } = false;
 
         [Required]
-        public string UserId { get; set; }
+        public required string UserId { get; set; }
 
         // helps to create a relationship between the JobPosting and the IdentityUser
         // reduces the need to write custom queries to get the user details
-        [ForeignKey(nameof(UserId)]
-        public IdentityUser User { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public required IdentityUser User { get; set; }
 
     }
 }
