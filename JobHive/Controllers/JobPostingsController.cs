0ee62cfffd60cc1485d1e3118775f412d77a2999
harrytimbog <harriemannie@gmail.com>
2024-10-14 21:45:35 +0100

    Creating the Job Postings Controller

---
+++ JobHive/Controllers/JobPostingsController.cs
@@ -0,0 +1,26 @@
+﻿using JobHive.Models;
+using JobHive.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobHive.Controllers
+{
+    public class JobPostingsController : Controller
+    {
+
+        private readonly IRepository<JobPosting> _jobPostingRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public JobPostingsController(
+            IRepository<JobPosting> jobRepository,
+            UserManager<IdentityUser> userManager)
+        {
+            _jobPostingRepository = jobRepository;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- JobHive/Program.cs
+++ JobHive/Program.cs
@@ -61,6 +61,6 @@ app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=JobPostings}/{action=Index}/{id?}");
 
 app.Run();
